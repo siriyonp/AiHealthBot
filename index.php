e852720b25afab4c884f6e00093b24adc03adf28
snology <sphutthachan@gmail.com>
2017-11-15 13:39:31 +0700

    Initial...

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php
-  require_once('./vendor/autoload.php');
-  use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-  use \LINE\LINEBot;
-  use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+      require_once('./vendor/autoload.php');
 
-  $channel_token = 'mKHjsIuCJYsy79zw+5IgVdw1TgHZH40q2TTxjmFoHJKT81Ws1z8s9TBDo3ldaHjbdPYC4h0o0zRYh3HICn/UQ5f/0Vo/7Wh9qTQdyAdy1aWlA+GU8WiCVGD37b9bPf+a56tt10kaNCPnQv7BbMuT/gdB04t89/1O/w1cDnyilFU=';
-  $channel_secret = '7b8abd826a3faa5168785b1220c90586';
+      use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+      use \LINE\LINEBot;
+      use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+      $channel_token = 'mKHjsIuCJYsy79zw+5IgVdw1TgHZH40q2TTxjmFoHJKT81Ws1z8s9TBDo3ldaHjbdPYC4h0o0zRYh3HICn/UQ5f/0Vo/7Wh9qTQdyAdy1aWlA+GU8WiCVGD37b9bPf+a56tt10kaNCPnQv7BbMuT/gdB04t89/1O/w1cDnyilFU=';
+      $channel_secret = '4ccf33748e78c7cb555b373a9da71e86';
+          //Get message from Line API
+          $content = file_get_contents('php://input'); $events=json_decode($content, true);
+                if (!is_null($events['events'])) { //Loop through each event
+                  foreach($events['events']as $event){
+                    //Line API send a lot of event type, we interested in message only.
+                    if($event['type']=='message' && $event['message']['type']=='text'){
+                    //Get replyToken
+                    $replyToken = $event['replyToken'];
+                    //Split message then keep it in database. $appointments=explode(',', $event['message']['text']);
+                    if(count($appointments) == 2) {
+                    $host = 'ec2-174-129-223-193.compute-1.amazonaws.com';
+                    $dbname = 'd74bjtc28mea5m';
+                    $user = 'eozuwfnzmgflmu';
 
-  //Get message from Line API
-  $content = file_get_contents('php://input');
-  $events=json_decode($content, true);
-
-  if (!is_null($events['events'])) { //Loop through each event
-      foreach($events['events']as $event){
-        //Get replyToken
-        $replyToken = $event['replyToken']; $ask = $event['message']['text'];
-          switch(strtolower($ask)) { case 'm':
-          $respMessage='What sup man.Go away!';
-            break; case 'f':
-          $respMessage='Love you lady.';
-            break; default:
-          $respMessage='What is your sex? M or F'; break;
-        }
-  $httpClient = new CurlHTTPClient($channel_token);
-  $bot=newLINEBot($httpClient, array('channelSecret'=> $channel_secret)); $textMessageBuilder = new TextMessageBuilder($respMessage);
-  $response=$bot->replyMessage($replyToken, $textMessageBuilder);
-    }
- }
+                $pass = '2340614a293db8e8a8c02753cd5932cdee45ab90bfcc19d0d306754984cbece1';
+                $connection=newPDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+                $params = array(
+                'time'=> $appointments[0], 'content'=> $appointments[1],
+                );
+          $statement=$connection->prepare("INSERT INTO appointments (time, content)VALUES(:time, $result = $statement->execute($params);
+          $respMessage='Your appointment has saved.'; }else{
+          $respMessage='You can send appointment like this "12.00,House keeping."'; }
+          $httpClient = new CurlHTTPClient($channel_token); $bot=newLINEBot($httpClient, array('channelSecret'=> $channel_secret));
+          $textMessageBuilder = new TextMessageBuilder($respMessage); $response=$bot->replyMessage($replyToken, $textMessageBuilder);
+          } }
+}
+echo "OK";
